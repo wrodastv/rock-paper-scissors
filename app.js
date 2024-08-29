c86d8d561f9a8a279e45946c009c63ec8a9ba57e
Willie Rodas <wrodas@naboo.attlocal.net>
2024-08-29 16:34:07 -0400

    start necesary files for the game

---
+++ app.js
@@ -0,0 +1,13 @@
+//Ask User for rock paper or scissors
+function getComputerChoice() {
+	return 'Rock';
+}
+
+let computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+// store it in a variable
+
+// create a random math function to come up with a random number from 1 - 3
+
+// create a if else statement to print to console if you won or lost
