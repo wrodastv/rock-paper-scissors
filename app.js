df05f9005bae02db2a5f461ff4689b64c8695970
Willie Rodas <wrodas@naboo.attlocal.net>
2024-08-29 21:26:25 -0400

    Add functions to clean up code

--- app.js
+++ app.js
@@ -1,81 +1,89 @@
 console.log('Rock, Paper, Scissor Shoot...');
-let humanWins = 0;
-let computerWins = 0;
-while (computerWins < 5 && humanWins < 5) {
-	//Computer Choice
-	function getComputerChoice() {
-		let i = Math.round(Math.random() * 2) + 1;
-
-		let choice;
-		if (i === 1) {
-			choice = 'rock';
-		} else if (i === 2) {
-			choice = 'paper';
-		} else if (i === 3) {
-			choice = 'scissor';
-		}
-		return choice;
-	}
-
-	function getHumanChoice() {
-		let choice;
-		let i;
-		while (i != 0) {
-			choice = prompt('Rock, Paper or Scissor?');
-			choice = choice.toLocaleLowerCase();
-
-			if (choice.localeCompare('rock') === 0) {
-				i = 0;
-			} else if (choice.localeCompare('paper') === 0) {
-				i = 0;
-			} else if (choice.localeCompare('scissor') === 0) {
-				i = 0;
-			} else {
-				i = 1;
-			}
-		}
-		return choice;
-	}
-
-	// create a if else statement to print to console if you won or lost
-	console.log('Computer Choice: ' + computerChoice);
-	console.log('Your Choice: ' + humanChoice);
-
-	if (humanChoice.localeCompare(computerChoice) == 0) {
-		console.log('Looks like you tied');
-	} else if (humanChoice.localeCompare('rock') === 0) {
-		if (computerChoice.localeCompare('paper') === 0) {
-			console.log('You Lost');
-			computerWins++;
-		} else if (computerChoice.localeCompare('scissor') === 0) {
-			console.log('You Won');
-			humanWins++;
-		}
-	} else if (humanChoice.localeCompare('paper') === 0) {
-		if (computerChoice.localeCompare('scissor') === 0) {
-			console.log('You Lost');
-			computerWins++;
-		} else if (computerChoice.localeCompare('rock') === 0) {
-			console.log('You Won');
-			humanWins++;
-		}
-	} else {
-		if (computerChoice.localeCompare('rock') === 0) {
-			console.log('You Lost');
-			computerWins++;
-		} else if (computerChoice.localeCompare('paper') === 0) {
-			console.log('You Won');
-			humanWins++;
-		}
-	}
-	console.log('Computer Wins: ' + computerWins);
-	console.log('Your Wins: ' + humanWins);
+let humanScore = 0;
+let computerScore = 0;
+//Computer Choice
+function getComputerChoice() {
+	let i = Math.round(Math.random() * 2) + 1;
+	return i;
 }
 
-if (computerWins > humanWins) {
-	console.log('You have lost against the computer');
+function getHumanChoice() {
+	let choice;
+	let i = 0;
+	while (i === 0) {
+		choice = prompt('Rock, Paper or Scissor?');
+		choice = choice.toLocaleLowerCase();
+
+		if (choice.localeCompare('rock') === 0) {
+			i = 1;
+		} else if (choice.localeCompare('paper') === 0) {
+			i = 2;
+		} else if (choice.localeCompare('scissor') === 0) {
+			i = 3;
+		} else {
+			i = 0;
+			console.log('You must choose between Rock Paper or Scissor');
+		}
+	}
+	return i;
+}
+
+function playRound(humanSelection, computerSelection) {
+	if (humanSelection === computerSelection) {
+		console.log('You have Tied');
+	} else if (humanSelection === 1) {
+		if (computerSelection === 2) {
+			console.log('You Lost! Paper beats Rock');
+			computerScore++;
+		} else if (computerSelection === 3) {
+			console.log('You Won! Rock beats Scissor');
+			humanScore++;
+		}
+	} else if (humanSelection === 2) {
+		if (computerSelection === 3) {
+			console.log('You Lost! Scissors Beat Paper');
+			computerScore++;
+		} else if (computerSelection === 1) {
+			console.log('You Won! Paper Beats Rock');
+			humanScore++;
+		}
+	} else if (humanSelection === 3) {
+		if (computerSelection === 1) {
+			console.log('You Lost! Rock Beats Scissors');
+			computerScore++;
+		} else if (computerSelection === 2) {
+			console.log('You Won! Scissors beat paper');
+			humanScore++;
+		}
+	}
+}
+
+function playGame() {
+	let gameRounds = 0;
+	let humanChoice;
+	let computerChoice;
+	while (gameRounds < 5) {
+		humanChoice = getHumanChoice();
+		computerChoice = getComputerChoice();
+		playRound(humanChoice, computerChoice);
+
+		console.log('Computer Score: ' + computerScore);
+		console.log('Your Score: ' + humanScore);
+		gameRounds++;
+	}
+}
+
+playGame();
+
+console.log('Computer Final Score: ' + computerScore);
+console.log('Your Final Score: ' + humanScore);
+
+if (computerScore > humanScore) {
+	console.log('Computer Wins!!');
+} else if (humanScore > computerScore) {
+	console.log('You Win!!');
 } else {
-	console.log('You have won against the computer');
+	console.log('You Tied. Great Effort');
 }
 
 console.log('Thanks for playing');
